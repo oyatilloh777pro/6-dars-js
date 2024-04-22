a5c6edf702406565731fa789134cc199c874229b
Oyatilloh <oyatillohqobiljonov81@gmail.com>
2024-04-22 11:34:25 +0500

    done

---
+++ index.js
@@ -0,0 +1,29 @@
+let word = prompt("Hafta kunlarini yozing!");
+
+if (word === "Monday"){
+    alert("Dushanba");
+}
+
+if (word === "Tuesday"){
+    alert("Seshanba");
+}
+
+if (word === "wendnasday"){
+    alert("Chorshanba");
+}
+
+if (word === "thrusday"){
+    alert("Payshanba");
+}
+
+if (word === "Friday"){
+    alert("Juma");
+}
+
+if (word === "Saturasday"){
+    alert("Shanba");
+}
+
+if (word === "Sunday"){
+    alert("Yakshanba");
+}
